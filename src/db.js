ce9c78bb4d27d29e309e41d9f51e97270a78c8f2
Prakash Mishra <pkm20002mis@gmail.com>
2025-12-15 18:03:09 +0530

    enable ssl for render postgres

--- src/db.js
+++ src/db.js
@@ -1,9 +1,52 @@
+// const { Sequelize } = require('sequelize');
+// const sequelize = new Sequelize(process.env.DATABASE_URL, {
+//   dialect: 'postgres',
+//   logging: false
+// });
+
+// const User = require('./models/user')(sequelize);
+// const Group = require('./models/group')(sequelize);
+// const Membership = require('./models/membership')(sequelize);
+// const Property = require('./models/property')(sequelize);
+// const Expense = require('./models/expense')(sequelize);
+// const Contribution = require('./models/contribution')(sequelize);
+// const SellRequest = require('./models/sellRequest')(sequelize);
+// const Transaction = require('./models/transaction')(sequelize);
+
+// // Associations
+// Group.hasMany(Membership, { foreignKey: 'groupId' });
+// Membership.belongsTo(Group, { foreignKey: 'groupId' });
+
+// User.hasMany(Membership, { foreignKey: 'userId' });
+// Membership.belongsTo(User, { foreignKey: 'userId' });
+
+// Group.hasMany(Property, { foreignKey: 'groupId' });
+// Property.belongsTo(Group, { foreignKey: 'groupId' });
+
+// Group.hasMany(Expense, { foreignKey: 'groupId' });
+// Expense.belongsTo(Group, { foreignKey: 'groupId' });
+
+// Group.hasMany(Transaction, { foreignKey: 'groupId' });
+// Transaction.belongsTo(Group, { foreignKey: 'groupId' });
+
+// User.hasMany(Transaction, { foreignKey: 'userId' });
+// Transaction.belongsTo(User, { foreignKey: 'userId' });
+
+// module.exports = { sequelize, User, Group, Membership, Property, Expense, Contribution, SellRequest, Transaction };
 const { Sequelize } = require('sequelize');
+
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
-  logging: false
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 });
 
+// ================= MODELS =================
 const User = require('./models/user')(sequelize);
 const Group = require('./models/group')(sequelize);
 const Membership = require('./models/membership')(sequelize);
@@ -13,7 +56,7 @@ const Contribution = require('./models/contribution')(sequelize);
 const SellRequest = require('./models/sellRequest')(sequelize);
 const Transaction = require('./models/transaction')(sequelize);
 
-// Associations
+// ================= ASSOCIATIONS =================
 Group.hasMany(Membership, { foreignKey: 'groupId' });
 Membership.belongsTo(Group, { foreignKey: 'groupId' });
 
@@ -32,4 +75,14 @@ Transaction.belongsTo(Group, { foreignKey: 'groupId' });
 User.hasMany(Transaction, { foreignKey: 'userId' });
 Transaction.belongsTo(User, { foreignKey: 'userId' });
 
-module.exports = { sequelize, User, Group, Membership, Property, Expense, Contribution, SellRequest, Transaction };
+module.exports = {
+  sequelize,
+  User,
+  Group,
+  Membership,
+  Property,
+  Expense,
+  Contribution,
+  SellRequest,
+  Transaction
+};
